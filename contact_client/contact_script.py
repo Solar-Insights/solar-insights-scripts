dc79a80587a4c8930167588c9b3a56aeb3d34f6f
MathBeaudoin <mathisbeaudoin15@hotmail.com>
2024-10-04 18:43:27 -0400

    added organization type to script args

--- contact_client/contact_script.py
+++ contact_client/contact_script.py
@@ -2,6 +2,7 @@ from argparse import ArgumentParser, Namespace
 from typing import Literal
 
 LanguageChoices = Literal["fr", "en"]
+OrganizationType = Literal["energy", "public", "software"]
 
 def add_language_arg(parser: ArgumentParser) -> None:
     parser.add_argument(
@@ -11,13 +12,25 @@ def add_language_arg(parser: ArgumentParser) -> None:
         required=True,
         action="store", 
         dest="language", 
-        help="The desired language of contact"
+        help="Language of contact"
+    )
+
+def add_org_type_arg(parser: ArgumentParser) -> None:
+    parser.add_argument(
+        "-ot", "--org-type", 
+        type=str,
+        choices=["energy", "public", "software"],
+        required=True,
+        action="store", 
+        dest="org_type", 
+        help="Type of organization"
     )
 
 def create_parser() -> ArgumentParser:
     parser = ArgumentParser(description="A script to generate contact texts for Solar Insights")
 
     add_language_arg(parser)
+    add_org_type_arg(parser)
 
     return parser
 
@@ -29,6 +42,8 @@ def get_parsed_args() -> Namespace:
 args: Namespace = get_parsed_args()
 
 
-LANGUAGE: LanguageChoices = args.language[0]
+LANGUAGE: LanguageChoices = args.language
+ORG_TYPE: OrganizationType = args.org_type
 
-print(args.language)
+print(LANGUAGE)
+print(ORG_TYPE)
