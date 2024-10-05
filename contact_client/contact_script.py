16a1b5d5f40b2ff808b1bf0511573ff2cb847d8c
MathBeaudoin <mathisbeaudoin15@hotmail.com>
2024-10-04 20:44:40 -0400

    corrections to fr text files, finalized the scrip with printing

--- contact_client/contact_script.py
+++ contact_client/contact_script.py
@@ -1,6 +1,7 @@
 from argparse import ArgumentParser, Namespace
 from typing import Literal
 
+
 LanguageChoices = Literal["fr", "en"]
 OrganizationType = Literal["energy", "public", "software"]
 
@@ -34,16 +35,57 @@ def create_parser() -> ArgumentParser:
 
     return parser
 
-
 def get_parsed_args() -> Namespace:
     parser = create_parser()
     return parser.parse_args()
 
-args: Namespace = get_parsed_args()
+def get_full_file_name(file_descriptor: str) -> str:
+    return f"{SUB_DIR}/{file_descriptor}-{LANGUAGE}.{TEXT_FILE_EXTENSION}"
 
 
-LANGUAGE: LanguageChoices = args.language
-ORG_TYPE: OrganizationType = args.org_type
+SUB_DIR = "./contact_client"
+ARGS: Namespace = get_parsed_args()
+LANGUAGE: LanguageChoices = ARGS.language
+ORG_TYPE: OrganizationType = ARGS.org_type
+TEXT_FILE_EXTENSION = "txt"
+FILES = {
+    "title": "title",
+    "introduction": "introduction",
+    "conclusion": "conclusion",
+    "public": "public_org",
+    "energy": "energy_org",
+    "software": "sofware_org"
+}
 
-print(LANGUAGE)
-print(ORG_TYPE)
+title = ""
+communication = ""
+
+title_file = get_full_file_name(FILES["title"])
+introduction_file = get_full_file_name(FILES["introduction"])
+org_file = get_full_file_name(FILES[ORG_TYPE])
+conclusion_file = get_full_file_name(FILES["conclusion"])
+
+with open(title_file, "r", encoding="utf-8") as f:
+    title += f.read()
+
+with open(introduction_file, "r", encoding="utf-8") as f:
+    communication += f.read()
+
+with open(org_file, "r", encoding="utf-8") as f:
+    communication += f.read()
+
+with open(conclusion_file, "r", encoding="utf-8") as f:
+    communication += f.read()
+
+org_name = input("organization name: ")
+
+communication = communication.replace("$org_name$", org_name)
+
+
+
+print(title)
+
+print(communication)
+
+
+
